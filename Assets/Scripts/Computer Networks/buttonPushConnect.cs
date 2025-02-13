205769c59a23e675734d0c6034f70bada38826d8
Merlan <merlan01tel@gmail.com>
2025-02-13 23:52:09 +0500

    init

---
+++ Assets/Scripts/Computer Networks/buttonPushConnect.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonPushConnect : MonoBehaviour
+{
+    public bool isCrossover = false;
+    public bool isStraight = false;
+    public Animator anim;
+    private GameObject textMeshPro;
+
+    private void Start()
+    {
+        textMeshPro = GameObject.FindGameObjectWithTag("TextCamera");
+    }
+
+    public void MakeCrossover()
+    {
+        Debug.Log("Crossover");
+        isCrossover = true;
+        isStraight = false;
+    }
+
+    public void MakeStraight()
+    {
+        Debug.Log("Straight");
+        isCrossover = false;
+        isStraight = true;
+    }
+
+    public void Connect(string str)
+    {
+        TextMeshPro textMain = textMeshPro.GetComponent<TextMeshPro>();
+        textMain.text = "";
+        if (str == "Computer->Commutator")
+        {
+            if (isStraight)
+            {
+                textMain.text = "Дұрыс қосылды";
+                anim.Play("InsertCabel");
+            }
+            else
+            {
+                textMain.text = "Қате қосылым";
+            }
+        }
+        else if (str == "Router->Commutator")
+        {
+            if (isStraight)
+            {
+                textMain.text = "Дұрыс қосылды";
+                anim.Play("InsertCabel");
+            }
+            else
+            {
+                textMain.text = "Қате қосылым";
+            }
+        }
+    }
+}
